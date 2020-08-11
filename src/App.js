985ae2e4817aed2a483affa20e82ab949181ef85
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-08-11 22:40:44 +0300

    Add image and styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Cards, Chart, CountryPicker } from "./components";
 import { fetchData } from "./api";
 import styles from "./App.module.css";
+import covidImage from "./images/image.png";
 
 class App extends Component {
   state = {
@@ -25,6 +26,7 @@ class App extends Component {
     const { data, country } = this.state;
     return (
       <div className={styles.container}>
+        <img src={covidImage} alt="covid-19" className={styles.image} />
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart data={data} country={country} />
